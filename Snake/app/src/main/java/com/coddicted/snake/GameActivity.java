b680ff91516f903494279d4e3a72f25f6eb57321
Anupam Jain <anupam2204.smart@gmail.com>
2017-08-28 21:09:01 +0530

    main activity animation working

---
+++ Snake/app/src/main/java/com/coddicted/snake/GameActivity.java
@@ -0,0 +1,14 @@
+package com.coddicted.snake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
